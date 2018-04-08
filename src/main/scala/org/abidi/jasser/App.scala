c92cd809869ea27de165268de044d61b75d282c7
EliteBook <jacerabidi@gmail.com>
2018-04-08 22:37:21 +0200

    refactoring

--- src/main/scala/org/abidi/jasser/App.scala
+++ src/main/scala/org/abidi/jasser/App.scala
@@ -10,7 +10,8 @@ object App {
   def main(args : Array[String])={
     val spark = SparkSession.builder().appName("test").master("local[2]").getOrCreate()
     import spark.implicits._
-    (1 to 100).toDF().show
+    val df = (1 to 100).toDF()
+      df.show
   }
   println( "Hello World!" )
 }
