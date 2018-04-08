c4086e3dafe48321417c34a1c51ce09176b2b954
EliteBook <jacerabidi@gmail.com>
2018-04-08 23:42:12 +0200

    declare spark session

--- src/main/scala/org/abidi/jasser/App.scala
+++ src/main/scala/org/abidi/jasser/App.scala
@@ -8,6 +8,7 @@ package org.abidi.jasser
 import org.apache.spark.sql.SparkSession
 object App {
   def main(args : Array[String])={
+    // declare sparksession
     val spark = SparkSession.builder().appName("test").master("local[2]").getOrCreate()
     import spark.implicits._
     val df = (1 to 100).toDF()
